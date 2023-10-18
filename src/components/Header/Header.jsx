ebe8cffacf8b07796134b2ef1406ed31afac2b7e
Daniel Dushev <danieldyshew@gmail.com>
2023-10-18 17:01:29 +0300

    Style Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
+import './Header.css';
 
 export default function Header() {
     return (
         <header className="header">
             <div className="header__logo">
                 <a href="/" className="header__nav-logo">
-                    <img src="/logo.png" alt="House logo" />
+                    <img className="header__logo-img" src="/logo.png" alt="House logo" />
                 </a>
             </div>
             <nav className="header__navigation">
-                <ul>
-                    <li className="header__list-item">
+                <ul className="header__nav-list">
+                    <li>
                         <a className="header__nav-link" href="/catalog">Catalog</a>
                     </li>
-                    <li className="header__list-item">
+                    <li>
+                        <a className="header__nav-link" href="/catalog">Create</a>
+                    </li>
+                    <li>
                         <a className="header__nav-link" href="/logout">Logout</a>
                     </li>
-                    <li className="header__list-item">
+                    <li>
                         <a className="header__nav-link" href="/login">Login</a>
                     </li>
-                    <li className="header__list-item">
+                    <li>
                         <a className="header__nav-link" href="/register">Register</a>
                     </li>
                 </ul>
