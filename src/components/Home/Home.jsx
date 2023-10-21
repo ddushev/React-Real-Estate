cc01cfc362395aec9b1d7f5f03dcc619534b3674
Daniel Dushev <danieldyshew@gmail.com>
2023-10-21 21:40:14 +0300

    add listings container

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,11 @@ import './Home.css';
 export default function Home() {
     return (
         <div className="homepage-featured">
-            <h2 className="homepage-featured__heading">Explore our top Homes</h2>
+            <h2 className="homepage-featured__heading">Explore our top Properties</h2>
             <div className="homepage-featured__list">
-                
+                <div className="homepage-featured_listing">
+
+                </div>
             </div>
         </div>
     );
