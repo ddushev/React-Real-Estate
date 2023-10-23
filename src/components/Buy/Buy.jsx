f27cc87a78923d3b70dbae63a7ba0f0909538464
Daniel Dushev <danieldyshew@gmail.com>
2023-10-23 19:14:00 +0300

    Add Buy component strucutre

--- src/components/Buy/Buy.jsx
+++ src/components/Buy/Buy.jsx
@@ -2,6 +2,14 @@ import './Buy.css'
 
 export default function Buy() {
     return (
-        <h1>Buy page</h1>
+        <section className="buy-section">
+            <div className="buy-section__properties-container">
+
+            </div>
+            <div className="buy-section__img-container">
+                <img className="sell-section__img" src="./dark-skyscraper.jpg" alt="dark-skyscraper" />
+            </div>
+
+        </section>
     );
 }
