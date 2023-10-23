6130f7e17794a2094b318899d5c4eee2ec891b3a
Daniel Dushev <danieldyshew@gmail.com>
2023-10-23 17:28:18 +0300

    Add Sell component basic layout and styling

--- src/components/Sell/Sell.jsx
+++ src/components/Sell/Sell.jsx
@@ -1,7 +1,26 @@
 import './Sell.css';
 
 export default function Sell() {
-    return(
-        <h1>Sell page</h1>
+    return (
+        <section className="sell-section">
+            <div className="sell-section__img-container">
+                <img className="sell-section__img" src="./dark-skyscraper.jpg" alt="dark-skyscraper" />
+            </div>
+            <div className="sell-section__form-container">
+                <form method="POST" className="sell-section__form">
+                    <label className="sell-section__title-label" htmlFor="title">Title</label>
+                    <input className="sell-section__title-input" type="text" id="title" name="title" placeholder=""/>
+                    <label className="sell-section__address-label" htmlFor="address">Address</label>
+                    <input className="sell-section__address-input" type="text" id="address" name="address" placeholder=""/>
+                    <label className="sell-section__type-label" htmlFor="type">Type</label>
+                    <input className="sell-section__type-input" type="text" id="type" name="type" placeholder=""/>
+                    <label className="sell-section__price-label" htmlFor="price">Starting price</label>
+                    <input className="sell-section__price-input" type="number" name="price" id="price" placeholder=""/>
+                    <label className="sell-section__description-label" htmlFor="description">Type</label>
+                    <textarea className="sell-section__description-label" name="description" id="description" cols="30" rows="10" placeholder="">
+                    </textarea>
+                </form>
+            </div>
+        </section>
     );
 }
